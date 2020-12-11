289872e4f3a7ae0d485d79e4d3315e4b410a0ddd
cyoq <di9akoz@gmail.com>
2020-12-11 17:47:38 +0200

    fixed default printing

--- maze_solver/src/dip107/Labyrinth.java
+++ maze_solver/src/dip107/Labyrinth.java
@@ -503,8 +503,8 @@ public class Labyrinth {
             for (int j = 0; j < width + 1; j++) {
                 if (i > 0 && i < height + 1 && j > 0 && j < width + 1) {
                     switch (maze[i - 1][j - 1]) {
-                        case 0 -> System.out.print("0");
-                        case 1 -> System.out.print("1");
+                        case 0 -> System.out.print("0 ");
+                        case 1 -> System.out.print("1 ");
                     }
                 }
             }
